d2a46089d1efa718ec124f97a837ebe2d5b219ca
fabalcu97 <fabalcu97@gmail.com>
2025-02-22 01:39:36 -0300

    feat: add status field to Todo model with choices for task management

--- todos/models.py
+++ todos/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 
 class Todo(models.Model):
+    class Status(models.TextChoices):
+        TODO = "TODO", "Todo"
+        IN_PROGRESS = "IN_PROGRESS", "In Progress"
+        DONE = "DONE", "Done"
+
     name = models.CharField(max_length=255, null=False)  # Required name
-    description = models.TextField(blank=True, null=True)  # Optional description
-    priority = models.IntegerField(null=False)  # Required priority
+    status = models.CharField(
+        max_length=20, choices=Status.choices, default=Status.TODO, null=False
+    )  # Required status
+    # Optional description
+    description = models.TextField(blank=True, null=True)
+    # Required priority
+    priority = models.IntegerField(null=False)
     order_number = models.IntegerField(null=True, blank=True)  # Optional order number
     created_at = models.DateTimeField(
         auto_now_add=True

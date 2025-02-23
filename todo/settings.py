111ae6d9e47a90b206cc9b465e85eb5afe8c1f56
fabalcu97 <fabalcu97@gmail.com>
2025-02-23 18:12:11 -0300

    feat: update ALLOWED_HOSTS to include localhost for local development

--- todo/settings.py
+++ todo/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
